5ceea2b5b9d0c699887c96fa1c6bc1b48f133b50
Betto74 <s21120191@alumnos.itsur.edu.mx>
2024-08-26 22:21:21 -0600

    new

---
+++ apiRest/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+public static class WebApiConfig
+{
+    public static void Register(HttpConfiguration config)
+    {
+        // Habilitar CORS globalmente
+        var cors = new EnableCorsAttribute("*", "*", "*");
+        config.EnableCors(cors);
+
+        // Configuración y servicios de API web
+        // Configuración de rutas de API web
+        config.MapHttpAttributeRoutes();
+
+        config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+        );
+    }
+}
---
+++ apiRest/Controllers/tacosController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using apiRest.Models;
+
+namespace apiRest.Controllers
+{
+    public class tacosController : ApiController
+    {
+        private TaqueriaEntities db = new TaqueriaEntities();
+
+        // GET: api/tacos
+        public IQueryable<tacos> Gettacos()
+        {
+            return db.tacos;
+        }
+
+        // GET: api/tacos/5
+        [ResponseType(typeof(tacos))]
+        public IHttpActionResult Gettacos(int id)
+        {
+            tacos tacos = db.tacos.Find(id);
+            if (tacos == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tacos);
+        }
+
+        // PUT: api/tacos/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Puttacos(int id, tacos tacos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tacos.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tacos).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tacosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/tacos
+        [ResponseType(typeof(tacos))]
+        public IHttpActionResult Posttacos(tacos tacos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tacos.Add(tacos);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tacos.id }, tacos);
+        }
+
+        // DELETE: api/tacos/5
+        [ResponseType(typeof(tacos))]
+        public IHttpActionResult Deletetacos(int id)
+        {
+            tacos tacos = db.tacos.Find(id);
+            if (tacos == null)
+            {
+                return NotFound();
+            }
+
+            db.tacos.Remove(tacos);
+            db.SaveChanges();
+
+            return Ok(tacos);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tacosExists(int id)
+        {
+            return db.tacos.Count(e => e.id == id) > 0;
+        }
+    }
+}
